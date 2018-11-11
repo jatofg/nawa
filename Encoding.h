4a4f949f9a11b448fff798f49590003ba4ac5115
Tobias Flaig <me@tobias-flaig.de>
2018-11-12 00:26:52 +0100

    Change to char32_t in Encoding to support more characters (untested)

--- Encoding.h
+++ Encoding.h
@@ -13,7 +13,23 @@ namespace Qsf {
      * Namespace containing functions for text encoding and decoding
      */
     namespace Encoding {
+        /**
+         * Escape special HTML characters in the input string. Only utf-8 strings are supported by this function.
+         * @param input String with characters that need to be escaped for use in HTML.
+         * @param encodeAll If false (by default), only the characters &, ", <, and > will be escaped.
+         * If true, all characters that have an HTML entity will be encoded (this is less efficient, prefer to use
+         * utf-8 encoding consistently to avoid encoding problems and only escape the required characters).
+         * @return String with escaped characters.
+         */
         std::string htmlEncode(std::string input, bool encodeAll = false);
+        /**
+         * Decode a string with HTML entities by replacing them with the corresponding utf-8 characters.
+         * Only utf-8 strings are supported by this function.\n
+         * Only entities starting with & and ending in ; will be replaced, including the unicode notations.
+         * If the semicolon is missing, the entity will not be replaced.
+         * @param input Input string containing HTML entities.
+         * @return String in which the entities are decoded to utf-8 characters.
+         */
         std::string htmlDecode(std::string input);
         // TODO url encoding, base64 encoding, ...
     }
