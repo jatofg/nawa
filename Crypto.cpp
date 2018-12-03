dabfed0431aa7c6668e469743226f088b15f9e4c
Tobias Flaig <me@tobias-flaig.de>
2018-12-04 00:02:28 +0100

    Add SHA-2 and MD5 functions in Crypto

--- Crypto.cpp
+++ Crypto.cpp
@@ -5,6 +5,7 @@
 #include "Crypto.h"
 #include "Utils.h"
 #include <openssl/sha.h>
+#include <openssl/md5.h>
 
 std::string Qsf::Crypto::sha1(const std::string &input, bool hex) {
     auto sha1Base = (const unsigned char*) input.c_str();
@@ -12,6 +13,66 @@ std::string Qsf::Crypto::sha1(const std::string &input, bool hex) {
     size_t sha1Size = input.size();
     SHA1(sha1Base, sha1Size, sha1Hash);
     std::string ret((char*) sha1Hash, SHA_DIGEST_LENGTH);
+    if(hex) {
+        return hex_dump(ret);
+    }
+    return ret;
+}
+
+std::string Qsf::Crypto::sha224(const std::string &input, bool hex) {
+    auto sha2Base = (const unsigned char*) input.c_str();
+    unsigned char sha2Hash[SHA224_DIGEST_LENGTH];
+    size_t sha2Size = input.size();
+    SHA224(sha2Base, sha2Size, sha2Hash);
+    std::string ret((char*) sha2Hash, SHA224_DIGEST_LENGTH);
+    if(hex) {
+        return hex_dump(ret);
+    }
+    return ret;
+}
+
+std::string Qsf::Crypto::sha256(const std::string &input, bool hex) {
+    auto sha2Base = (const unsigned char*) input.c_str();
+    unsigned char sha2Hash[SHA256_DIGEST_LENGTH];
+    size_t sha2Size = input.size();
+    SHA256(sha2Base, sha2Size, sha2Hash);
+    std::string ret((char*) sha2Hash, SHA256_DIGEST_LENGTH);
+    if(hex) {
+        return hex_dump(ret);
+    }
+    return ret;
+}
+
+std::string Qsf::Crypto::sha384(const std::string &input, bool hex) {
+    auto sha2Base = (const unsigned char*) input.c_str();
+    unsigned char sha2Hash[SHA384_DIGEST_LENGTH];
+    size_t sha2Size = input.size();
+    SHA384(sha2Base, sha2Size, sha2Hash);
+    std::string ret((char*) sha2Hash, SHA384_DIGEST_LENGTH);
+    if(hex) {
+        return hex_dump(ret);
+    }
+    return ret;
+}
+
+std::string Qsf::Crypto::sha512(const std::string &input, bool hex) {
+    auto sha2Base = (const unsigned char*) input.c_str();
+    unsigned char sha2Hash[SHA512_DIGEST_LENGTH];
+    size_t sha2Size = input.size();
+    SHA512(sha2Base, sha2Size, sha2Hash);
+    std::string ret((char*) sha2Hash, SHA512_DIGEST_LENGTH);
+    if(hex) {
+        return hex_dump(ret);
+    }
+    return ret;
+}
+
+std::string Qsf::Crypto::md5(const std::string &input, bool hex) {
+    auto md5Base = (const unsigned char*) input.c_str();
+    unsigned char md5Hash[MD5_DIGEST_LENGTH];
+    size_t md5Size = input.size();
+    MD5(md5Base, md5Size, md5Hash);
+    std::string ret((char*) md5Hash, MD5_DIGEST_LENGTH);
     if(hex) {
         return hex_dump(ret);
     }
--- Crypto.h
+++ Crypto.h
@@ -9,15 +9,54 @@
 
 namespace Qsf {
     namespace Crypto {
-        // TODO more functions
         /**
-         * Get SHA1 hash of a string.
+         * Get SHA-1 hash of a string.
          * @param input String to hash.
          * @param hex If true (default), get hash in hexadecimal representation (40 chars). Otherwise, the hash will
          * be binary (20 bytes).
-         * @return SHA1 hash of input.
+         * @return SHA-1 hash of input.
          */
         std::string sha1(const std::string& input, bool hex = true);
+        /**
+         * Get SHA-224 hash of a string.
+         * @param input String to hash.
+         * @param hex If true (default), get hash in hexadecimal representation. Otherwise, the hash will
+         * be binary (28 bytes).
+         * @return Hash of input.
+         */
+        std::string sha224(const std::string &input, bool hex = true);
+        /**
+         * Get SHA-256 hash of a string.
+         * @param input String to hash.
+         * @param hex If true (default), get hash in hexadecimal representation. Otherwise, the hash will
+         * be binary (32 bytes).
+         * @return Hash of input.
+         */
+        std::string sha256(const std::string &input, bool hex = true);
+        /**
+         * Get SHA-384 hash of a string.
+         * @param input String to hash.
+         * @param hex If true (default), get hash in hexadecimal representation. Otherwise, the hash will
+         * be binary (48 bytes).
+         * @return Hash of input.
+         */
+        std::string sha384(const std::string &input, bool hex = true);
+        /**
+         * Get SHA-512 hash of a string.
+         * @param input String to hash.
+         * @param hex If true (default), get hash in hexadecimal representation. Otherwise, the hash will
+         * be binary (64 bytes).
+         * @return Hash of input.
+         */
+        std::string sha512(const std::string &input, bool hex = true);
+        /**
+         * Get MD5 hash of a string.
+         * @param input String to hash.
+         * @param hex If true (default), get hash in hexadecimal representation (32 chars). Otherwise, the hash will
+         * be binary (16 bytes).
+         * @return Hash of input.
+         */
+        std::string md5(const std::string &input, bool hex = true);
     };
 }
 
