86e31bb86d272114bd4f0807726399abe84da27a
Tobias Flaig <me@tobias-flaig.de>
2020-11-05 21:40:11 +0100

    Make HttpRequestHandler serve actual app content

--- src/RequestHandlers/HttpRequestHandler.cpp
+++ src/RequestHandlers/HttpRequestHandler.cpp
@@ -24,6 +24,7 @@
 #include <nawa/RequestHandlers/HttpRequestHandler.h>
 #include <nawa/Exception.h>
 #include <boost/network/protocol/http/server.hpp>
+#include <nawa/Connection.h>
 
 using namespace nawa;
 using namespace std;
@@ -32,36 +33,65 @@ struct HttpHandler;
 using HttpServer = http::server<HttpHandler>;
 
 struct HttpHandler {
-    void operator()(HttpServer::request const& request, HttpServer::connection_ptr connection) {
-        HttpServer::string_type ip = source(request);
-        unsigned int port = request.source_port;
-        std::ostringstream data;
-        data << "Hello, " << ip << ':' << port << '!';
-        connection->set_status(HttpServer::connection::ok);
-        connection->write(data.str());
+    RequestHandler *requestHandler = nullptr;
+    Config config;
+
+    void operator()(HttpServer::request const &request, HttpServer::connection_ptr httpConn) {
+        // TODO use read / create read callback to read headers (and hopefully environment?)
+        //      - outsource parsing POST, use POST parsing in fastcgi handler too, then?
+        //      - for environment, headers: the request object should have some interesting members
+        // TODO use request handling function for returning result
+        // TODO handle the fact that this library sets headers and status itself (splitting or modification?)
+
+        // TODO fill
+        RequestInitContainer requestInit;
+        ConnectionInitContainer connectionInit;
+        connectionInit.requestInit = move(requestInit);
+        connectionInit.config = config;
+
+        connectionInit.flushCallback = [&httpConn](const string &raw) {
+            httpConn->write(raw);
+        };
+
+        Connection connection(connectionInit);
+        requestHandler->handleRequest(connection);
+        connection.flushResponse();
+
     }
 };
 
 struct nawa::HttpRequestHandler::HttpHandlerAdapter {
     unique_ptr<HttpHandler> handler;
     unique_ptr<HttpServer> server;
+    int concurrency = 1;
     vector<thread> threadPool;
 };
 
 HttpRequestHandler::HttpRequestHandler(HandleRequestFunction handleRequestFunction, Config config_,
-                                             int concurrency) {
+                                       int concurrency) {
     setAppRequestHandler(move(handleRequestFunction));
     setConfig(move(config_));
 
     httpHandler = make_unique<HttpHandlerAdapter>();
     httpHandler->handler = make_unique<HttpHandler>();
+    httpHandler->handler->requestHandler = this;
+    httpHandler->handler->config = config;
     HttpServer::options httpServerOptions(*httpHandler->handler);
-    // TODO set options (host, port, ...) from config
-    httpHandler->server = make_unique<HttpServer>(httpServerOptions.port("8080"));
+
+    // set options from config
+    string listenAddr = config[{"http", "listen"}].empty() ? "127.0.0.1" : config[{"http", "listen"}];
+    string listenPort = config[{"http", "port"}].empty() ? "8080" : config[{"http", "port"}];
+    bool reuseAddr = (config[{"http", "reuseaddr"}] != "off");
+    httpHandler->server = make_unique<HttpServer>(
+            httpServerOptions.address(listenAddr).port(listenPort).reuse_address(reuseAddr));
+
+    if (concurrency > 0) {
+        httpHandler->concurrency = concurrency;
+    }
 
     try {
         httpHandler->server->listen();
-    } catch (exception const& e) {
+    } catch (exception const &e) {
         throw Exception(__PRETTY_FUNCTION__, 1,
                         string("Could not listen to host/port: ") + e.what());
     }
@@ -72,9 +102,10 @@ HttpRequestHandler::~HttpRequestHandler() = default;
 void HttpRequestHandler::start() {
     if (httpHandler && httpHandler->server) {
         try {
-            httpHandler->threadPool.emplace_back([this]{httpHandler->server->run();});
-            // TODO num threads according to config, save thread somewhere?
-        } catch (exception const& e) {
+            for (int i = 0; i < httpHandler->concurrency; ++i) {
+                httpHandler->threadPool.emplace_back([this] { httpHandler->server->run(); });
+            }
+        } catch (exception const &e) {
             throw Exception(__PRETTY_FUNCTION__, 1,
                             string("An error occurred during start of request handling:") + e.what());
         }
@@ -98,7 +129,7 @@ void HttpRequestHandler::terminate() noexcept {
 
 void HttpRequestHandler::join() noexcept {
     if (httpHandler) {
-        for(auto& t: httpHandler->threadPool) {
+        for (auto &t: httpHandler->threadPool) {
             t.join();
         }
     }
